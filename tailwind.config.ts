e0e4ed0371658b89de6c352fb1f5b79679a5321d
Pardip Bhatti <pardipbhatti8791@gmail.com>
2024-03-12 10:18:11 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        color: {
+          1: "#315d6e",
+          2: "#466d7c",
+          3: "#6cdbe6"
+        },
+        bar: {
+          1: "#819da8"
+        }
+      }
     },
   },
   plugins: [],
